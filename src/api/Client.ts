93b9f848d631c6e4944df279a36589a0b33eb3e9
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-08-21 20:09:17 +0500

    refactor: correct code according to eslint

--- src/api/Client.ts
+++ src/api/Client.ts
@@ -136,5 +136,5 @@ const getApiRoot = () => {
   return createApiBuilderFromCtpClient(currentApiClient).withProjectKey({ projectKey });
 };
 
-export { getDefaultApiClient, getAnonApiClient, getUserApiClient, currentApiClient }
+export { getDefaultApiClient, getAnonApiClient, getUserApiClient, currentApiClient };
 export default getApiRoot;
--- src/api/api.test.tsx
+++ src/api/api.test.tsx
@@ -1,14 +1,20 @@
-import { projectKey, FlowTypes } from "./Client";
-import { getDefaultApiClient, getAnonApiClient, getUserApiClient } from "./Client";
-import { changeApiClient, currentApiClient } from "./Client";
-import { makePersistentTokenCache } from "./TokenCache";
+import {
+  projectKey,
+  FlowTypes,
+  getDefaultApiClient,
+  getAnonApiClient,
+  getUserApiClient,
+  changeApiClient,
+  currentApiClient,
+} from './Client';
+import { makePersistentTokenCache } from './TokenCache';
 
 test('The correct key is used for the project in production', () => {
   expect(projectKey).toBe('trp');
 });
 
 test('Flow types switcher works correctly', () => {
-  const trialUserCreds = {username: 'John', password: 'JustJohn#1'};
+  const trialUserCreds = { username: 'John', password: 'JustJohn#1' };
 
   changeApiClient(FlowTypes.DEFAULT);
   expect(JSON.stringify(currentApiClient)).toBe(JSON.stringify(getDefaultApiClient()));
@@ -23,7 +29,7 @@ test('Flow types switcher works correctly', () => {
 test('Get ans set methods for tokenCache', () => {
   const current = {};
   const tokenKey = 'token';
-  const newToken = { token: '123-456-789', expirationTime: 300};
+  const newToken = { token: '123-456-789', expirationTime: 300 };
   makePersistentTokenCache(tokenKey).set(newToken);
 
   expect(localStorage.getItem(tokenKey)).toBe(JSON.stringify({ token: '123-456-789', expirationTime: 300 }));
