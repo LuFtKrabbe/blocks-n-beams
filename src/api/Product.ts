86199f5147d15323adef8d4148f5f30b9aea92f9
Nevold <Nevold@tut.by>
2023-09-04 20:49:43 +0300

    refactor: change category structure

--- src/api/Product.ts
+++ src/api/Product.ts
@@ -3,10 +3,11 @@ import { ClientResponse, Product, ProductProjectionPagedSearchResponse } from '@
 import getApiRoot from './Client';
 
 export default class ProductApi {
-  static MAIN_LINK_ID = '48df60e0-fafd-42b9-a970-2e5bb9915016';
-  static SQUARE_LINK_ID = '19fbc664-7671-4d1f-9f9d-a4d40e02fbf8';
-  static CIRCLE_LINK_ID = '48df60e0-fafd-42b9-a970-2e5bb9915016';
-  static DELTA_LINK_ID = 'c8e9a09c-2a45-465a-8825-381b76187c85';
+  static BRICS_LINK_ID = '48df60e0-fafd-42b9-a970-2e5bb9915016';
+  static AEROCRETE_LINK_ID = '19fbc664-7671-4d1f-9f9d-a4d40e02fbf8';
+  static REINFORCED_CONCRETE_LINK_ID = '7a39fa28-6605-4935-8f96-36529add45c1';
+  static TIMBER_LINK_ID = 'b168b7a8-5d33-4f5f-9a25-1532bef0b9fd';
+  static AGGREGATES_LINK_ID = 'c8e9a09c-2a45-465a-8825-381b76187c85';
 
   static getCategoriesById = (categoriesId: string): Promise<ClientResponse<ProductProjectionPagedSearchResponse>> => {
     return getApiRoot()
@@ -23,5 +24,16 @@ export default class ProductApi {
 
   static getCardById = (cardId: string): Promise<ClientResponse<Product>> => {
     return getApiRoot().products().withId({ ID: cardId }).get().execute();
+  };
+  static getCards = (): Promise<ClientResponse<ProductProjectionPagedSearchResponse>> => {
+    return getApiRoot()
+      .productProjections()
+      .search()
+      .get({
+        queryArgs: {
+          limit: 20,
+        },
+      })
+      .execute();
   };
 }
