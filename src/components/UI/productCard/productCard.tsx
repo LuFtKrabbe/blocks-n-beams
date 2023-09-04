35e4495b344d2b26615b03fc07ce2a06d3ba7d76
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-09-04 03:50:21 +0300

    refactor: style the card parts

--- src/components/UI/productCard/productCard.tsx
+++ src/components/UI/productCard/productCard.tsx
@@ -9,6 +9,8 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { userSlice } from '../../../app/reducers';
 
+import styles from './productCard.module.css';
+
 const ProductCard: FC<{ productCardList: ProductProjection }> = ({ productCardList }): JSX.Element => {
   const navigate = useNavigate();
   // const categories = useAppSelector((state) => state.categories);
@@ -19,12 +21,15 @@ const ProductCard: FC<{ productCardList: ProductProjection }> = ({ productCardLi
 
   const productImage = productCardList.masterVariant.images?.[0].url;
   const productDescription = productCardList.metaDescription?.['en-US'];
+  const isPriceDiscount = productCardList.metaDescription?.['en-US'];
+
   const productPrice = () => {
     const price = productCardList.masterVariant.prices?.[0].value;
     return price ? (price.centAmount / 100).toFixed(2) : 'No price';
   }
   const productPriceDiscount = () => {
     const priceDiscount = productCardList.masterVariant.prices?.[0].discounted?.value;
+    priceDiscount
     return priceDiscount ? (priceDiscount.centAmount / 100).toFixed(2) : 'No discount';
   }
 
@@ -38,25 +43,36 @@ const ProductCard: FC<{ productCardList: ProductProjection }> = ({ productCardLi
     // navigate(`/main/${currentLocationCardId}/${productCardList.id}`);
   };
   return (
-    <Card
-      title={productCardList.name['en-US']}
-      hoverable
-      style={{ width: 300, marginTop: 10 }}
-      extra={<a onClick={onClick}>View details</a>}
-      cover={<Image src={productImage ? productImage : 'No image'} />}
-    >
-      <Meta description={productDescription ? productDescription : 'No description for this product'} />
-      <Meta
-        style={{ marginTop: 10 }}
-        avatar={<EuroOutlined style={{ fontSize: 20 }} />}
-        description={
-          <>
-            <span> {productPrice()} </span>
-            <span> {productPriceDiscount()} </span>
-          </>
+    <div className={styles.cardWrapper}>
+      <Card
+        title={productCardList.name['en-US']}
+        bordered
+        hoverable
+        style={{ width: 250, height: 480 }}
+        extra={<a onClick={onClick}>View details</a>}
+        cover={
+          <Image
+            style={{padding: 2}}
+            src={productImage ? productImage : 'No image'}
+            height={240}
+          />
         }
-      />
-    </Card>
+      >
+        <Meta 
+          style={{ marginBottom: 10, height: 100 }}
+          description={productDescription ? productDescription : 'No description for this product'} />
+        <Meta
+          style={{ marginTop: 10 }}
+          avatar={<EuroOutlined style={{ fontSize: 23 }} />}
+          description={
+            <div className={styles.prices}>
+              <span className={(productPriceDiscount() == 'No discount') ? styles.price : styles.priceOff}> {productPrice()} </span>
+              <span className={(productPriceDiscount() == 'No discount') ? styles.priceDiscountOff : styles.priceDiscount}> {productPriceDiscount()} </span>
+            </div>
+          }
+        />
+      </Card>
+    </div>
   );
 };
 
