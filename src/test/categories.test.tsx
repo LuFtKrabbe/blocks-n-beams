130df19b7756ad50733e9a4e123cd642469676a7
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-09-19 00:01:18 +0300

    feat: add tests for all product categories

--- src/test/categories.test.tsx
+++ src/test/categories.test.tsx
@@ -4,9 +4,14 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Aerocrete from '../pages/categories/aerocrete';
+import Aggregates from '../pages/categories/aggregates';
+import Beams from '../pages/categories/beams';
+import Blocks from '../pages/categories/blocks';
+import Bricks from '../pages/categories/bricks';
+import ReinforcedConcrete from '../pages/categories/reinforcedConcrete';
+import Timber from '../pages/categories/timber';
 
 import { store } from '../app/store';
-import Aggregates from '../pages/categories/aggregates';
 
 describe('App', () => {
   beforeEach(() => {
@@ -49,7 +54,77 @@ describe('App', () => {
     expect(await findByText('Vermiculite', { exact: true })).toBeInTheDocument();
     expect(await findByText('Gravel', { exact: true })).toBeInTheDocument();
   });
+  
+  test('Beams category renders succesfully', async () => {
+    const { findByText } = render(
+      <Provider store={store}>
+        <Router>
+          <Beams />;
+        </Router>,
+      </Provider>
+    );
+  
+    console.log = function() {}; //remove output of the response and request from server
+  
+    expect(await findByText('Wooden plank', { exact: true })).toBeInTheDocument();
+    expect(await findByText('Bulkhead', { exact: true })).toBeInTheDocument();
+  });
+  
+  test('Blocks category renders succesfully', async () => {
+    const { findByText } = render(
+      <Provider store={store}>
+        <Router>
+          <Blocks />;
+        </Router>,
+      </Provider>
+    );
+  
+    console.log = function() {}; //remove output of the response and request from server
+  
+    expect(await findByText('Perforated clay facing brick', { exact: true })).toBeInTheDocument();
+  });
 
+  test('Bricks category renders succesfully', async () => {
+    const { findByText } = render(
+      <Provider store={store}>
+        <Router>
+          <Bricks />;
+        </Router>,
+      </Provider>
+    );
+  
+    console.log = function() {}; //remove output of the response and request from server
+  
+    expect(await findByText('Solid thickened clay brick', { exact: true })).toBeInTheDocument();
+  });
+
+  test('ReinforcedConcrete category renders succesfully', async () => {
+    const { findByText } = render(
+      <Provider store={store}>
+        <Router>
+          <ReinforcedConcrete />;
+        </Router>,
+      </Provider>
+    );
+  
+    console.log = function() {}; //remove output of the response and request from server
+  
+    expect(await findByText('Foundation beam', { exact: true })).toBeInTheDocument();
+  });
+
+  test('Timber category renders succesfully', async () => {
+    const { findByText } = render(
+      <Provider store={store}>
+        <Router>
+          <Timber />;
+        </Router>,
+      </Provider>
+    );
+  
+    console.log = function() {}; //remove output of the response and request from server
+  
+    expect(await findByText('Glued beam', { exact: true })).toBeInTheDocument();
+  });
 });
 
 
